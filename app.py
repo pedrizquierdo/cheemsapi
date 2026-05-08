2a932e6cf73dfd714a4e1381023d33ff8996adea
pedrizquierdo <pedro.izquierdo3005@gmail.com>
2026-05-07 19:10:07 -0700

    Add update and delete endpoints for trip management

--- app.py
+++ app.py
@@ -21,7 +21,16 @@ def save_trip():
     success = id is not None
     return jsonify(success), 201
 
+@app.route('/trip/<int:id>', methods=['PUT'])
+def update_trip(id):
+    data = request.json
+    success = Trip.update(id, data['name'], data['city'], data['latitude'], data['longitude'])
+    return jsonify(success), 200
 
+@app.route('/trip/<int:id>', methods=['DELETE'])
+def delete_trip(id):
+    success = Trip.delete(id)
+    return jsonify(success), 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5001)))
